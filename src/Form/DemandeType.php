bfdbfd66623c53f7d3309493e14eaa4762a997cf
youssef el mahdi bouchouicha <youssefelmahdi.bouchouicha@esprit.tn>
2021-08-18 00:31:19 +0100

    gestion demande

--- src/Form/DemandeType.php
+++ src/Form/DemandeType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Application;
 use App\Entity\Client;
 use App\Entity\Demande;
+use App\Entity\Employee;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -16,7 +17,20 @@ class DemandeType extends AbstractType
     {
         $builder
 
+            ->add('idc' ,EntityType::class, [
+                // looks for choices from this entity
+                'class' => Client::class,
 
+                'choice_label' => 'nom',
+
+            ])
+            ->add('ide' ,EntityType::class, [
+                // looks for choices from this entity
+                'class' => Employee::class,
+
+                'choice_label' => 'nom',
+
+            ])
             ->add('idapp' ,EntityType::class, [
                 // looks for choices from this entity
                 'class' => Application::class,
